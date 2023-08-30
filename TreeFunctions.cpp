fe8c46d3a6f5fdb0e2798de834b198a5d8aebfbd
martinhejo <marhjoh@stud.ntnu.no>
2023-08-31 01:28:04 +0200

    Add: countBigger function

--- TreeFunctions.cpp
+++ TreeFunctions.cpp
@@ -24,3 +24,21 @@ void TreeFunctions::writeEveryOtherLeafNode(Node* node){
         writeEveryOtherLeafNode(node->left);       // Check nodes at the left
     }
 }
+
+/**
+ * Counts the number of nodes in a binary tree that have values greater than a specified value.
+ *
+ * @param node A pointer to the root of the binary tree or subtree to be counted.
+ * @param value The threshold value against which node values are compared.
+ * @return The count of nodes with values greater than 'value' in the binary tree or subtree.
+ */
+int TreeFunctions::countBigger(Node* node, int value){
+    if(node){                                               // If there is a node there, the code is executed
+        if(value < node->ID && value != node->ID){          // If a node is larger than the value should be
+            return countBigger(node->left, value) +   // +1 is returned
+            countBigger(node->right, value) + 1;
+        } else{
+            return 0;                                       // Returns 0 if there are no nodes larger
+        }
+    }
+}
--- TreeFunctions.h
+++ TreeFunctions.h
@@ -6,6 +6,7 @@
 class TreeFunctions {
 public:
     static void writeEveryOtherLeafNode(Node* node);
+    static int countBigger(Node* node, int value);
 };
 
 #endif // TREEFUNCTIONS_H
